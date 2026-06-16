3ceb24d815aafdfff55f4643d365d3257934c55b
CodedByShxm <shemr@live.com>
2026-06-15 23:32:14 -0500

    feat: enhance ExchangeRateClient to fetch latest FX rates and improve error handling

--- src/main/java/com/joukou/fxexchangeservice/client/ExchangeRateClient.java
+++ src/main/java/com/joukou/fxexchangeservice/client/ExchangeRateClient.java
@@ -1,8 +1,11 @@
 package com.joukou.fxexchangeservice.client;
 
+import com.joukou.fxexchangeservice.config.FxProperties;
 import com.joukou.fxexchangeservice.dto.ExchangeResponse;
+import com.joukou.fxexchangeservice.dto.LatestRatesResponse;
 import com.joukou.fxexchangeservice.exception.ExternalServiceException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -10,14 +13,14 @@ import org.springframework.web.client.RestClient;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ExchangeRateClient {
 
     private final RestClient restClient;
+    private final FxProperties fxProperties;
 
-    @Value("${fx.api.key}")
-    private String apiKey;
 
     @Cacheable(value = "fx-rates", key = "#from + '_' + #to")
     public BigDecimal getRate(String from, String to) {
@@ -29,7 +32,7 @@ public class ExchangeRateClient {
                                     .scheme("https")
                                     .host("api.exchangerate.host")
                                     .path("/convert")
-                                    .queryParam("access_key", apiKey)
+                                    .queryParam("access_key", fxProperties.getApi().getKey())
                                     .queryParam("from", from)
                                     .queryParam("to", to)
                                     .queryParam("amount", 1)
@@ -37,6 +40,8 @@ public class ExchangeRateClient {
                             .retrieve()
                             .body(ExchangeResponse.class);
 
+            log.info(fxProperties.getApi().getKey());
+            log.info(response.toString());
             if (response == null || response.getResult() == null) {
                 throw new ExternalServiceException("Invalid FX response from provider");
             }
@@ -47,6 +52,40 @@ public class ExchangeRateClient {
             throw new ExternalServiceException(
                     "Failed to fetch exchange rate: " + ex.getMessage()
             );
+        }
+    }
+
+    public LatestRatesResponse getLatestRates(String baseCurrency) {
+
+        try {
+
+            log.info("Fetching latest FX rates for base currency {}", baseCurrency);
+
+            LatestRatesResponse response =
+                    restClient.get()
+                            .uri(uriBuilder -> uriBuilder
+                                    .scheme("https")
+                                    .host("api.exchangerate.host")
+                                    .path("/latest")
+                                    .queryParam("base", baseCurrency)
+                                    .queryParam("access_key", fxProperties.getApi().getKey())
+                                    .build())
+                            .retrieve()
+                            .body(LatestRatesResponse.class);
+
+            if (response == null || response.getRates() == null) {
+                throw new ExternalServiceException(
+                        "Invalid response from FX provider");
+            }
+
+            return response;
+
+        } catch (Exception ex) {
+
+            log.error("Failed to retrieve FX rates", ex);
+
+            throw new ExternalServiceException(
+                    "Unable to retrieve exchange rates");
         }
     }
 }
