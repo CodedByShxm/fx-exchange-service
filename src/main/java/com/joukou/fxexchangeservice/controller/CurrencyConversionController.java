a38a217650c4cd2da5756be09528cd8c75adc0b1
CodedByShxm <shemr@live.com>
2026-06-15 23:32:35 -0500

    feat: add Swagger annotations for FX Conversion API documentation

--- src/main/java/com/joukou/fxexchangeservice/controller/CurrencyConversionController.java
+++ src/main/java/com/joukou/fxexchangeservice/controller/CurrencyConversionController.java
@@ -2,10 +2,13 @@ package com.joukou.fxexchangeservice.controller;
 
 import com.joukou.fxexchangeservice.dto.*;
 import com.joukou.fxexchangeservice.service.CurrencyConversionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "FX Conversion API")
 @RestController
 @RequestMapping("/api/v1/conversions")
 @RequiredArgsConstructor
@@ -13,6 +16,7 @@ public class CurrencyConversionController {
 
     private final CurrencyConversionService service;
 
+    @Operation(summary = "Convert currency")
     @PostMapping
     public ConversionResponse convert(
             @Valid @RequestBody ConversionRequest request
